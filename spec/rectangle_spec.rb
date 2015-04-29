f0ce8a85ce18e9b6f322fcf6953c809700acdb32
Ian MacDonald & Franz Knupfer <student@epicodus.com>
2015-04-29 09:24:02 -0700

    write square? and initialize Ruby programs, spec passes

---
+++ spec/rectangle_spec.rb
@@ -0,0 +1,12 @@
+require('rspec')
+require('pry')
+require('rectangle')
+
+describe('Rectangle') do
+  describe('#square?') do
+    it('will return false if the rectangle is not a square') do
+      test_rectangle = Rectangle.new(40, 30)
+      expect(test_rectangle.square?()).to(eq(false))
+    end
+  end
+end
